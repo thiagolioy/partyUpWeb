3900222528f1a856d8501c70579e0fdfadfb466c
Thiago Lioy <lioyufrj@gmail.com>
2015-01-28 09:55:18 -0200

    refactoring

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,145 +1,167 @@
 var PartyUp = PartyUp || (function() {
+
   var file;
   var place;
 
-  var bindUploadButtonEvent = function () {
-    $('#upload-button').click(function(){
-      $('#select-file').click();
-    });
+
+  var appConfig = {
+    parseAppId : "5sjv0ulSUoP2jMeLfvblBcfWhAkhQ76bDwRVxnh6",
+    parseRestApiKey : "497ZfHyTwSgrhbnaYSCZiOrS8p3k0HpvtIRrKyXM",
+    parseJsApiKey : "tLOzhEPIJpzTvtQ0SszzwLv1lFC8nsucaePUc7YO"
+
   };
 
-  var bindRequestFocusOnDatepicker = function(){
-    $('#datepicker').focus(function(){
-      $('#datepicker').fdatepicker('show');
-    });
+
+  var eventBindings = {
+
+    bindUploadButtonEvent : function () {
+      $('#upload-button').click(function(){
+        $('#select-file').click();
+      });
+    },
+
+    bindRequestFocusOnDatepicker : function(){
+      $('#datepicker').focus(function(){
+        $('#datepicker').fdatepicker('show');
+      });
+    },
+
+    bindChangePartyTypeEvent : function () {
+      alert("bindChangePartyTypeEvent");
+      $('#select-party-list-type').bind("change",function(f){
+        var option = $('#select-party-list-type').val();
+
+        if(option == "facebook"){
+          $("#facebook-event-id").parent().removeClass("hide");
+          $("#event-email").parent().addClass("hide");
+        }else if(option == "email"){
+          $("#event-email").parent().removeClass("hide");
+          $("#facebook-event-id").parent().addClass("hide");
+        }
+      });
+    },
+
+    bindSelectFileEvent : function () {
+      $('#select-file').bind("change", function(f) {
+        var files = f.target.files || f.dataTransfer.files;
+        var name = files[0].name.toLowerCase();
+        var contains = (name.indexOf(".png") > -1 || name.indexOf(".jpg") > -1 || name.indexOf(".jpeg") > -1);
+
+        if(contains){
+          file = files[0];
+          $("#filename").val(file.name);
+        }else{
+
+        }
+      });
+    },
+
+    bindSavePartyEvent : function(){
+      $('#save-button').click(function() {
+        actions.postParty();
+      });
+    },
+
+    attachEvents : function () {
+      eventBindings.bindUploadButtonEvent();
+      eventBindings.bindSelectFileEvent();
+      eventBindings.bindSavePartyEvent();
+      eventBindings.bindChangePartyTypeEvent();
+      eventBindings.bindRequestFocusOnDatepicker();
+    }
+
   };
 
-  var bindChangePartyTypeEvent = function () {
-    $('#select-party-list-type').bind("change",function(f){
-      var option = $('#select-party-list-type').val();
+  var UIUtils = {
+    toggleBounceAnimation : function(elId,turnOn){
 
-      if(option == "facebook"){
-        $("#facebook-event-id").parent().removeClass("hide");
-        $("#event-email").parent().addClass("hide");
-      }else if(option == "email"){
-        $("#event-email").parent().removeClass("hide");
-        $("#facebook-event-id").parent().addClass("hide");
-      }
-    });
-  };
-
-  var bindSelectFileEvent = function () {
-    $('#select-file').bind("change", function(f) {
-      var files = f.target.files || f.dataTransfer.files;
-      var name = files[0].name.toLowerCase();
-      var contains = (name.indexOf(".png") > -1 || name.indexOf(".jpg") > -1 || name.indexOf(".jpeg") > -1);
-
-      if(contains){
-        file = files[0];
-        $("#filename").val(file.name);
+      if(turnOn){
+        $(elId).removeClass("hide");
+        $(elId).removeClass('animated bounceOutUp');
+        $(elId).addClass('animated bounceInDown');
       }else{
-
+        $(elId).removeClass('animated bounceInDown');
+        $(elId).addClass('animated bounceOutUp');
+        $(elId).addClass('hide');
       }
-    });
+    },
+
+    updateProgressBar : function(evt){
+      var percentComplete = Math.round((evt.loaded / evt.total)* 100);
+      var status = "" + percentComplete + "%";
+      $("#progressbar-meter").animate({width:status});
+    }
+
+
   };
 
-  var toggleBounceAnimation = function(elId,turnOn){
+  var actions = {
 
-    if(turnOn){
-      $(elId).removeClass("hide");
-      $(elId).removeClass('animated bounceOutUp');
-      $(elId).addClass('animated bounceInDown');
-    }else{
-      $(elId).removeClass('animated bounceInDown');
-      $(elId).addClass('animated bounceOutUp');
-      $(elId).addClass('hide');
+    uploadFile : function(){
+      var serverUrl = 'https://api.parse.com/1/files/' + file.name;
+      $.ajax({
+        xhr: function() {
+          var xhr = new window.XMLHttpRequest();
+          xhr.upload.addEventListener("progress", function(evt) {
+            if (evt.lengthComputable) {
+              updateProgressBar(evt);
+            }
+          }, false);
+          return xhr;
+        },
+        type: "POST",
+        beforeSend: function(request) {
+          request.setRequestHeader("X-Parse-Application-Id", '5sjv0ulSUoP2jMeLfvblBcfWhAkhQ76bDwRVxnh6');
+          request.setRequestHeader("X-Parse-REST-API-Key", '497ZfHyTwSgrhbnaYSCZiOrS8p3k0HpvtIRrKyXM');
+          request.setRequestHeader("Content-Type", file.type);
+          UIUtils.toggleBounceAnimation("#progressbar-container",true);
+        },
+        url: serverUrl,
+        data: file,
+        processData: false,
+        contentType: false,
+        success: function(data) {
+          UIUtils.toggleBounceAnimation("#progressbar-container",false);
+          actions.postParty(file);
+        },
+        error: function(data) {
+          var obj = jQuery.parseJSON(data);
+          alert(obj.error);
+        }
+      });
+
+    },
+
+    postParty : function(){
+      var inputs = ["name","datepicker","description","select-place",
+      "facebook-event-id","event-email","male-price","female-price"];
+      var keys = ["name","date","description","placeId","facebookId",
+      "eventEmail","malePrice","femalePrice"];
+      var params = {};
+      $.each(inputs, function(i,v){
+        params[keys[i]] = $("#"+v).val();
+      });
+
+
+      $.ajax({
+        type: "POST",
+        url: "http://partyup.parseapp.com/party",
+        data: params,
+        contentType: "application/json",
+        success: function(data) {
+          alert("sucesso : " +data);
+        },
+        error: function(data) {
+          alert("error : " +data);
+        }
+      });
+
     }
   };
 
-  var updateProgressBar = function(evt){
-    var percentComplete = Math.round((evt.loaded / evt.total)* 100);
-    var status = "" + percentComplete + "%";
-    $("#progressbar-meter").animate({width:status});
-  };
-
-  var uploadFile = function(){
-    var serverUrl = 'https://api.parse.com/1/files/' + file.name;
-    $.ajax({
-      xhr: function() {
-        var xhr = new window.XMLHttpRequest();
-        xhr.upload.addEventListener("progress", function(evt) {
-          if (evt.lengthComputable) {
-            updateProgressBar(evt);
-          }
-        }, false);
-        return xhr;
-      },
-      type: "POST",
-      beforeSend: function(request) {
-        request.setRequestHeader("X-Parse-Application-Id", '5sjv0ulSUoP2jMeLfvblBcfWhAkhQ76bDwRVxnh6');
-        request.setRequestHeader("X-Parse-REST-API-Key", '497ZfHyTwSgrhbnaYSCZiOrS8p3k0HpvtIRrKyXM');
-        request.setRequestHeader("Content-Type", file.type);
-        toggleBounceAnimation("#progressbar-container",true);
-      },
-      url: serverUrl,
-      data: file,
-      processData: false,
-      contentType: false,
-      success: function(data) {
-        toggleBounceAnimation("#progressbar-container",false);
-        postParty(file);
-      },
-      error: function(data) {
-        var obj = jQuery.parseJSON(data);
-        alert(obj.error);
-      }
-    });
-
-  };
-
-  var postParty = function(){
-    var inputs = ["name","datepicker","description","select-place",
-    "facebook-event-id","event-email","male-price","female-price"];
-    var keys = ["name","date","description","placeId","facebookId",
-    "eventEmail","malePrice","femalePrice"];
-    var params = {};
-    $.each(inputs, function(i,v){
-      params[keys[i]] = $("#"+v).val();
-    });
-
-
-    $.ajax({
-      type: "POST",
-      url: "http://partyup.parseapp.com/party",
-      data: params,
-      contentType: "application/json",
-      success: function(data) {
-        alert("sucesso : " +data);
-      },
-      error: function(data) {
-        alert("error : " +data);
-      }
-    });
-
-  };
-
-
-  var bindSavePartyEvent = function(){
-    $('#save-button').click(function() {
-      postParty();
-    });
-  };
-
-  var attachEvents = function () {
-    bindUploadButtonEvent();
-    bindSelectFileEvent();
-    bindSavePartyEvent();
-    bindChangePartyTypeEvent();
-    bindRequestFocusOnDatepicker();
-  };
 
   return {
-    attachEvents : attachEvents
+    attachEvents : eventBindings.attachEvents
   };
 })();
 
